3e48093e5fc4d8297ec73681313dad4ecc4e79f2
sainiboey <kabising@gmail.com>
2019-12-03 22:51:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,3 +25,12 @@ end
 def move(board,position,token)
   board[position] = token
 end
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+
+end
